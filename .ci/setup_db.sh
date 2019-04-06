cb84ba1a3f6bf444c228e07fb2d10df7d034ddbd
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-04-06 12:25:32 -0500

    fix db setup

--- .ci/setup_db.sh
+++ .ci/setup_db.sh
@@ -6,9 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PGUSER=postgres
 
-printf "[${pirogue_test}]\nhost=localhost\ndbname=${pirogue_test_db}\nuser=postgres\n\n" >> ~/.pg_service.conf
-dropdb --if-exists ${pirogue_test_db}
-createdb ${pirogue_test_db}
+printf "[pirogue_test]\nhost=localhost\ndbname=${pirogue_test_db}\nuser=postgres\n\n" >> ~/.pg_service.conf
+dropdb --if-exists pirogue_test_db
+createdb pirogue_test_db
 
 
 PGSERVICE=pirogue_test psql --quiet -v ON_ERROR_STOP=on -f ${DIR}/data/demo_data.sql;
