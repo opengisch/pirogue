c6bfa23427ab046fa56ce249661f9a9a07e9e5b3
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-04-22 09:45:42 -0500

    improved commands

--- pirogue/utils.py
+++ pirogue/utils.py
@@ -2,7 +2,7 @@
 
 from psycopg2.extensions import cursor
 from enum import Enum
-from .information_schema import columns, primary_key
+from pirogue.information_schema import columns, primary_key, default_value
 
 
 class InvalidColumn(Exception):
@@ -64,67 +64,12 @@ def column_alias(column: str,
 def select_columns(pg_cur: cursor,
                    table_schema: str,
                    table_name: str,
-                   table_alias: str=None,
                    table_type: str = 'table',
-                   remove_pkey: bool=True,
-                   skip_columns: list=[],
-                   remap_columns: dict = {},
-                   columns_on_top: list=[],
-                   columns_at_end: list=[],
-                   prefix: str= None,
-                   indent: int=2) -> str:
-    """
-
-    :param pg_cur: the psycopg cursor
-    :param table_schema: the schema
-    :param table_name: the name of the table
-    :param table_type: the type of table, i.e. view or table
-    :param table_alias: if not specified, table is used
-    :param remove_pkey: if True, the primary is removed from the list
-    :param skip_columns: list of columns to be skipped
-    :param remap_columns: dictionary to remap columns
-    :param columns_on_top: bring the columns to the front of the list
-    :param columns_at_end: bring the columns to the end of the list
-    :param prefix: add a prefix to the columns (do not applied to remapped columns)
-    :param indent: add an indent in front
-    :return:
-    """
-    cols = sorted(columns(pg_cur,
-                          table_schema=table_schema,
-                          table_name=table_name,
-                          table_type=table_type,
-                          remove_pkey=remove_pkey,
-                          skip_columns=skip_columns),
-                  key=lambda col: __column_priority(col))
-
-    # check arguments
-    for param, dict_or_list in {'remap_columns': remap_columns,
-                                'columns_on_top': columns_on_top,
-                                'columns_at_end': columns_at_end}.items():
-        for col in dict_or_list:
-            if col not in cols:
-                raise InvalidColumn('Invalid column in {param} paramater: "{tab}" has no column "{col}"'
-                                    .format(param=param, tab=table_name, col=col))
-
-
-    return ',\n'.join(['{indent}{table_alias}.{column}{col_alias}'
-                      .format(indent=indent*' ',
-                              table_alias=table_alias or table_name,
-                              column=col,
-                              col_alias=column_alias(col, remap_columns=remap_columns, prefix=prefix, prepend_as=True))
-                       for col in cols])
-
-
-def insert_command(pg_cur: cursor,
-                   table_schema: str,
-                   table_name: str,
-                   table_alias: str=None,
-                   table_type: str = 'table',
+                   table_alias: str = None,
                    remove_pkey: bool=True,
                    skip_columns: list=[],
                    comment_skipped: bool = True,
                    remap_columns: dict = {},
-                   insert_values: dict = {},
                    columns_on_top: list=[],
                    columns_at_end: list=[],
                    prefix: str= None,
@@ -139,11 +84,73 @@ def insert_command(pg_cur: cursor,
     :param remove_pkey: if True, the primary is removed from the list
     :param skip_columns: list of columns to be skipped
     :param comment_skipped: if True, skipped columns are written but commented, otherwise they are not written
+    If remove_pkey is True, the primary key will not be printed
+    :param remap_columns: dictionary to remap columns
+    :param columns_on_top: bring the columns to the front of the list
+    :param columns_at_end: bring the columns to the end of the list
+    :param prefix: add a prefix to the columns (do not applied to remapped columns)
+    :param indent: add an indent in front
+    :return:
+    """
+    cols = sorted(columns(pg_cur,
+                          table_schema=table_schema,
+                          table_name=table_name,
+                          table_type=table_type,
+                          remove_pkey=remove_pkey),
+                  key=lambda col: __column_priority(col))
+
+    # check arguments
+    for param, dict_or_list in {'skip_columns': skip_columns,
+                                'remap_columns': remap_columns,
+                                'columns_on_top': columns_on_top,
+                                'columns_at_end': columns_at_end}.items():
+        for col in dict_or_list:
+            if col not in cols:
+                raise InvalidColumn('Invalid column in {param} paramater: "{tab}" has no column "{col}"'
+                                    .format(param=param, tab=table_name, col=col))
+
+    return ',\n{indent}'\
+        .format(indent=indent*' ')\
+        .join(['{skip}{table_alias}.{column}{col_alias}'
+              .format(skip='-- ' if col in skip_columns else '',
+                      table_alias=table_alias or table_name,
+                      column=col,
+                      col_alias=column_alias(col, remap_columns=remap_columns, prefix=prefix, prepend_as=True))
+               for col in cols if (comment_skipped or col not in skip_columns)])
+
+def insert_command(pg_cur: cursor,
+                   table_schema: str,
+                   table_name: str,
+                   table_type: str = 'table',
+                   table_alias: str = None,
+                   remove_pkey: bool = True,
+                   coalesce_pkey: bool = False,
+                   skip_columns: list = [],
+                   comment_skipped: bool = True,
+                   remap_columns: dict = {},
+                   insert_values: dict = {},
+                   columns_on_top: list=[],
+                   columns_at_end: list=[],
+                   prefix: str = None,
+                   returning: str = None,
+                   indent: int = 2) -> str:
+    """
+
+    :param pg_cur: the psycopg cursor
+    :param table_schema: the schema
+    :param table_name: the name of the table
+    :param table_type: the type of table, i.e. view or table
+    :param table_alias: the alias of the table
+    :param remove_pkey: if True, the primary is removed from the list
+    :param coalesce_pkey: if True, the following expression is used to insert the primary key: COALESCE( NEW.{pkey}, {default_value} )
+    :param skip_columns: list of columns to be skipped
+    :param comment_skipped: if True, skipped columns are written but commented, otherwise they are not written
     :param remap_columns: dictionary to remap columns
     :param insert_values: dictionary of expression to be used at insert
     :param columns_on_top: bring the columns to the front of the list
     :param columns_at_end: bring the columns to the end of the list
     :param prefix: add a prefix to the columns (do not applied to remapped columns)
+    :param returning: returning command
     :param indent: add an indent in front
     :return:
     """
@@ -154,6 +161,8 @@ def insert_command(pg_cur: cursor,
                           table_type=table_type,
                           remove_pkey=remove_pkey),
                   key=lambda col: __column_priority(col))
+
+    pkey = primary_key(pg_cur, table_schema, table_name)
 
     # check arguments
     for param, dict_or_list in {'skip_columns': skip_columns,
@@ -166,32 +175,43 @@ def insert_command(pg_cur: cursor,
                 raise InvalidColumn('Invalid column in {param} paramater: "{tab}" has no column "{col}"'
                                     .format(param=param, tab=table_name, col=col))
 
-    return """{indent}INSERT INTO {s}.{t} (
+    def value(col):
+        if col in insert_values:
+            return '{val} -- {ori_col}'.format(val=insert_values[col], ori_col=col)
+        cal = column_alias(col, remap_columns=remap_columns,
+                                                       prefix=prefix, field_if_no_alias=True)
+        if col == pkey and coalesce_pkey:
+            return 'COALESCE( NEW.{cal}, {pk_def} )'.format(cal=cal,
+                                                            pk_def=default_value(pg_cur, table_schema, table_name, pkey))
+        else:
+            return 'NEW.{cal}'.format(cal=cal)
+
+    next_comma_printed_1 = [False]
+    next_comma_printed_2 = [False]
+    return """INSERT INTO {s}.{t} (
 {indent}      {cols} 
-{indent}) VALUES ( 
+{indent}  ) VALUES ( 
 {indent}      {new_cols}
-{indent});
+{indent}  ){returning};
 """.format(indent=indent*' ',
            s=table_schema,
            t=table_name,
            cols='\n{indent}    '
                 .format(indent=indent*' ')
-                .join(['{skip}{comma}{col}'.format(indent=indent*' ',
-                                                   skip='-- ' if col in skip_columns else '',
-                                                   comma=', ' if i else '',
-                                                   col=col)
-                            for i, col in enumerate(cols) if (comment_skipped or col not in skip_columns)]),
+                .join(['{skip}{comma}{col}'
+                      .format(indent=indent*' ',
+                              skip='-- ' if col in skip_columns else '',
+                              comma=', ' if __print_comma(next_comma_printed_1, col in skip_columns) else '',
+                              col=col)
+                       for col in cols if (comment_skipped or col not in skip_columns)]),
            new_cols='\n{indent}    '
                     .format(indent=indent*' ')
                     .join(['{skip}{comma}{value}'
                           .format(skip='-- ' if col in skip_columns else '',
-                                  comma=', ' if i else '',
-                                  value=insert_values.get(col,
-                                                          'NEW.{cal}'.format(cal=column_alias(col,
-                                                                                              remap_columns=remap_columns,
-                                                                                              prefix=prefix,
-                                                                                              field_if_no_alias=True))))
-                                for i, col in enumerate(cols) if (comment_skipped or col not in skip_columns)]))
+                                  comma=', ' if __print_comma(next_comma_printed_2, col in skip_columns) else '',
+                                  value=value(col))
+                           for col in cols if (comment_skipped or col not in skip_columns)]),
+           returning=' RETURNING {returning}'.format(indent=4*' ', returning=returning) if returning else '')
 
 
 def update_command(pg_cur: cursor,
@@ -250,14 +270,6 @@ def update_command(pg_cur: cursor,
                                     .format(param=param, tab=table_name, col=col))
 
     next_comma_printed = [False]
-    def print_comma(next_comma_printed: bool, is_skipped: bool) -> bool:
-        if is_skipped:
-            return next_comma_printed[0]
-        elif not next_comma_printed[0]:
-            next_comma_printed[0] = True
-            return False
-        else:
-            return True
 
     return """UPDATE {s}.{t} {a} SET
 {indent}    {cols}
@@ -271,14 +283,14 @@ def update_command(pg_cur: cursor,
                      .join(['{skip}{comma}{col} = {new_col}'
                                 .format(indent=indent*' ',
                                         skip='-- ' if col in skip_columns else '',
-                                        comma=', ' if print_comma(next_comma_printed, col in skip_columns) else '',
+                                        comma=', ' if __print_comma(next_comma_printed, col in skip_columns) else '',
                                         col=col,
                                         new_col=update_values.get(col,
                                                                   'NEW.{cal}'.format(cal=column_alias(col,
                                                                                                       remap_columns=remap_columns,
                                                                                                       prefix=prefix,
                                                                                                       field_if_no_alias=True))))
-                                for i, col in enumerate(cols) if (comment_skipped or col not in skip_columns)]),
+                                for col in cols if (comment_skipped or col not in skip_columns)]),
                 where_clause=where_clause or '{pkey} = {pkal}'.format(pkey=pkey,
                                                                       pkal=update_values.get(pkey,
                                                                                              'OLD.{cal}'.format(cal=column_alias(pkey,
@@ -299,3 +311,19 @@ def __column_priority(column: str, columns_on_top: list=[], columns_at_end: list
         return 2
     else:
         return 1
+
+
+def __print_comma(next_comma_printed: list, is_skipped: bool) -> bool:
+    """
+    Determines if a comma should be printed
+    :param next_comma_printed: a list with a single boolean (works by reference)
+    :param is_skipped:
+    :return:
+    """
+    if is_skipped:
+        return next_comma_printed[0]
+    elif not next_comma_printed[0]:
+        next_comma_printed[0] = True
+        return False
+    else:
+        return True
