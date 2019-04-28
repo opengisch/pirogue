3d2734bcff6e669773f69094dd426c1550e851b0
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-04-28 14:56:46 -0500

    use existing deploy step

--- docs/build_deploy.sh
+++ docs/build_deploy.sh
@@ -8,20 +8,20 @@ sed -i "s/__VERSION__/${TRAVIS_TAG}/" conf.py
 echo "building docs"
 make html
 
-mkdir publish && pushd publish
-
-echo "clone repo at gh-pages branch"
-git config --global user.email "qgisninja@gmail.com"
-git config --global user.name "Geo Ninja"
-git clone https://${GH_TOKEN}@github.com/opengisch/pirogue.git --depth 1 --branch gh-pages
-
-cp -R ../_build/html/* .
-
-echo "git add, commit push"
-git add .
-git commit -m "API docs for: $TRAVIS_TAG"
-
-git push -v origin HEAD:gh-pages
-
-popd
+# mkdir publish && pushd publish
+# 
+# echo "clone repo at gh-pages branch"
+# git config --global user.email "qgisninja@gmail.com"
+# git config --global user.name "Geo Ninja"
+# git clone https://${GH_TOKEN}@github.com/opengisch/pirogue.git --depth 1 --branch gh-pages
+# 
+# cp -R ../_build/html/* .
+# 
+# echo "git add, commit push"
+# git add .
+# git commit -m "API docs for: $TRAVIS_TAG"
+# 
+# git push -v origin HEAD:gh-pages
+# 
+# popd
 popd
