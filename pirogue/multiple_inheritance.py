46af615960bfd6be1a786236bcb847cd90de5a68
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-04-09 17:11:50 +0200

    monkey patch

--- pirogue/multiple_inheritance.py
+++ pirogue/multiple_inheritance.py
@@ -4,6 +4,9 @@ try:
     import psycopg
 except ImportError:
     import psycopg2 as psycopg
+    import psycopg2.sql as __sql
+
+    psycopg.sql = __sql
 
 from pirogue.exceptions import InvalidDefinition, TableHasNoPrimaryKey, VariableError
 from pirogue.information_schema import (
